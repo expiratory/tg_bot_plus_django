bb7d5ad3b432fd7162053fe0eee8b92d0bf67708
Alexandr Kilchevskiy <alexkill94@mail.ru>
2024-02-11 13:04:08 +0300

    added_pagination for categories and subcategories

--- tg_bot/categories.py
+++ tg_bot/categories.py
@@ -1,6 +1,9 @@
 from aiogram import types, F, Router
 from db import connect_to_db, close_db
 from aiogram.utils.keyboard import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.exceptions import TelegramBadRequest
+from os import getenv
+from dotenv import load_dotenv, find_dotenv
 
 
 router = Router()
@@ -11,7 +14,7 @@ async def catalog(message: types.Message):
     await categories(message)
 
 
-async def categories(message: types.Message):
+async def categories(message: types.Message, page: int=0, start: bool=True):
     cursor, conn = await connect_to_db()
     cursor.execute('SELECT id, name FROM public.categories_category')
     records = cursor.fetchall()
@@ -20,15 +23,42 @@ async def categories(message: types.Message):
         await message.answer(text="Категорий пока не существует :(")
 
     buttons_list = []
-    for item in records:
-        category_id = item[0]
-        category_name = item[1]
-        buttons_list.append([types.InlineKeyboardButton(
-            text=category_name,
-            callback_data=f'Категория {str(category_id)}')]
-        )
-    kb = types.InlineKeyboardMarkup(inline_keyboard=buttons_list)
-    await message.answer(
-        "Нажмите на кнопку, чтобы перейти к категории",
-        reply_markup=kb
-    )
+    page_size = int(getenv('GLOBAL_PAGINATION_PAGE_SIZE'))
+    number = page * page_size
+
+    for i in range(page_size):
+        try:
+            category_id = records[number+i][0]
+            category_name = records[number+i][1]
+            buttons_list.append([types.InlineKeyboardButton(
+                text=category_name,
+                callback_data=f'Категория {str(category_id)}')]
+            )
+        except IndexError:
+            pass
+
+    if len(buttons_list) != 0:
+        buttons_list.append([
+            types.InlineKeyboardButton(
+                text='Назад',
+                callback_data=f'Назад {page} {start} категории'
+            ),
+            types.InlineKeyboardButton(
+                text='Далее',
+                callback_data=f'Далее {page} {start} категории'
+            )
+        ])
+        kb = types.InlineKeyboardMarkup(inline_keyboard=buttons_list)
+
+        if start:
+            await message.answer(
+                "Нажмите на кнопку, чтобы перейти к категории",
+                reply_markup=kb
+            )
+        else:
+            try:
+                await message.edit_reply_markup(
+                    reply_markup=kb
+                )
+            except TelegramBadRequest:
+                pass
--- tg_bot/subcategories.py
+++ tg_bot/subcategories.py
@@ -2,14 +2,18 @@ from aiogram import types, F, Router
 from db import connect_to_db, close_db
 from categories import categories
 from aiogram.utils.keyboard import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.exceptions import TelegramBadRequest
+from os import getenv
+from dotenv import load_dotenv, find_dotenv
 
 
 router = Router()
 
 
 @router.callback_query(F.data.startswith("Категория"))
-async def subcategories(callback: types.CallbackQuery):
-    category_id = int(callback.data.split()[1])
+async def subcategories(callback: types.CallbackQuery, page: int=0, start: bool=True, category_id: int=None):
+    if category_id is None:
+        category_id = int(callback.data.split()[1])
     cursor, conn = await connect_to_db()
     cursor.execute(f'SELECT id, name FROM public.subcategories_subcategory WHERE category_id = {category_id}')
     records = cursor.fetchall()
@@ -20,15 +24,71 @@ async def subcategories(callback: types.CallbackQuery):
         await categories(callback.message)
 
     buttons_list = []
-    for item in records:
-        subcategory_id = item[0]
-        subcategory_name = item[1]
-        buttons_list.append([types.InlineKeyboardButton(
-            text=subcategory_name,
-            callback_data=f'Подкатегория {str(subcategory_id)}')]
-        )
-    kb = types.InlineKeyboardMarkup(inline_keyboard=buttons_list)
-    await callback.message.edit_text(
-        "Нажмите на кнопку, чтобы перейти к подкатегории",
-        reply_markup=kb
-    )
+    page_size = int(getenv('GLOBAL_PAGINATION_PAGE_SIZE'))
+    number = page * page_size
+
+    for i in range(page_size):
+        try:
+            subcategory_id = records[number+i][0]
+            subcategory_name = records[number+i][1]
+            buttons_list.append([types.InlineKeyboardButton(
+                text=subcategory_name,
+                callback_data=f'Подкатегория {str(subcategory_id)}')]
+            )
+        except IndexError:
+            pass
+
+    if len(buttons_list) != 0:
+        buttons_list.append([
+            types.InlineKeyboardButton(
+                text='Назад',
+                callback_data=f'Назад {page} {start} подкатегории {category_id}'
+            ),
+            types.InlineKeyboardButton(
+                text='Далее',
+                callback_data=f'Далее {page} {start} подкатегории {category_id}'
+            )
+        ])
+        kb = types.InlineKeyboardMarkup(inline_keyboard=buttons_list)
+
+        if start:
+            await callback.message.answer(
+                "Нажмите на кнопку, чтобы перейти к подкатегории",
+                reply_markup=kb
+            )
+        else:
+            try:
+                await callback.message.edit_reply_markup(
+                    reply_markup=kb
+                )
+            except TelegramBadRequest:
+                pass
+
+@router.callback_query(F.data.startswith("Назад"))
+async def backward(callback: types.CallbackQuery):
+    category = callback.data.split()[3] == 'категории'
+    subcategory = callback.data.split()[3] == 'подкатегории'
+    start = callback.data.split()[2]
+    if start:
+        start = False
+    page = int(callback.data.split()[1]) - 1 if int(callback.data.split()[1]) > 0 else 0
+    if category:
+        await categories(callback.message, page, start)
+    if subcategory:
+        category_id = callback.data.split()[4]
+        await subcategories(callback, page, start, category_id)
+
+
+@router.callback_query(F.data.startswith("Далее"))
+async def forward(callback: types.CallbackQuery):
+    category = callback.data.split()[3] == 'категории'
+    subcategory = callback.data.split()[3] == 'подкатегории'
+    start = callback.data.split()[2]
+    if start:
+        start = False
+    page = int(callback.data.split()[1]) + 1
+    if category:
+        await categories(callback.message, page, start)
+    if subcategory:
+        category_id = callback.data.split()[4]
+        await subcategories(callback, page, start, category_id)
